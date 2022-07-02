4314bbfd853b0184678da39f9e8dec744b377980
ujez <ujezujez@gmail.com>
2022-07-02 12:01:37 +0100

    little update

---
+++ createInstance.tf
@@ -0,0 +1,39 @@
+resource "aws_key_pair" "levelup_key2" {
+  key_name   = "levelup_key2"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+#Create AWS Instance
+resource "aws_instance" "DockerInstance" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.levelup_key2.key_name
+
+  tags = {
+    Name = "custom_instance2"
+  }
+
+}
+
+#EBS resource Creation
+resource "aws_ebs_volume" "ebs-volume-1" {
+  availability_zone = "us-east-1a"
+  size              = 50
+  type              = "gp2"
+
+  tags = {
+    Name = "Secondary Volume Disk"
+  }
+}
+
+#Atatch EBS volume with AWS Instance
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+  device_name = "/dev/xvdh"
+  volume_id   = aws_ebs_volume.ebs-volume-1.id
+  instance_id = aws_instance.DockerInstance.id
+}
+
+#GET YOUR PUBLIC IP
+output "public_ip" {
+  value = aws_instance.DockerInstance.public_ip
+}
